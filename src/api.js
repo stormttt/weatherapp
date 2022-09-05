72fbbd891d78af4bd53de781d8b29232162bef6a
stromttt <xyxiaoYU1992@gmail.com>
2022-09-05 19:21:52 +0800

    new

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@
 const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '6ac05b2e03mshf3c0f96ed5b1f45p1c43f2jsnd1e2078a853f',
+		//get apo key from OpenWeatherMap
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,6 +11,7 @@ const geoApiOptions = {
 const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 const WEATHER_API_URL="https://api.openweathermap.org/data/2.5"
-const WEATHER_API_KEY="433fad257f71f8841c3fd8881d4685ef"
+//get apo key from  RapidAPI
+const WEATHER_API_KEY=""
 
 export {geoApiOptions,GEO_API_URL,WEATHER_API_URL,WEATHER_API_KEY}
